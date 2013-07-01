31a4569ea837db6f434ba7272d4ffa61f91ba9b1
Arron <arronzhang@me.com>
2013-07-01 20:27:26 +0800

    Build script for service

--- build/service.customer.mk
+++ build/service.customer.mk
@@ -3,11 +3,11 @@ include build/config.mk
 PRODUCT_NAME = service
 
 JS_APP_FILES = ${JS_SRC_DIR}/ui.buddy.js\
-	${APP_SRC_DIR}/ui.visitorstatus.js\
-	${APP_SRC_DIR}/ui.logmsg.js\
+	${APP_SRC_DIR}/ui.layout.customer.js\
 
 CSS_APP_FILES = ${CSS_SRC_DIR}/buddy.css\
 	${CSS_SRC_DIR}/user.css\
 	${APP_SRC_DIR}/service.css\
+	${APP_SRC_DIR}/layout.customer.css\
 
 include build/include.mk
