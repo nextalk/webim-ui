be0a8e2f7615b7793a76da3cd433e3cac09ab118
Arron <arronzhang@me.com>
2013-07-16 16:29:57 +0800

    Change author

--- css/core.css
+++ css/core.css
@@ -2,7 +2,7 @@
  * Webim UI v@VERSION
  * http://www.webim20.cn/
  *
- * Copyright (c) 2010 Hidden
+ * Copyright (c) 2013 Arron
  * Released under the MIT, BSD, and GPL Licenses.
  *
  * Date: @DATE
