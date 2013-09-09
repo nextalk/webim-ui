cd2b2563c33e934887db8e3a901d8ffef0882ce4
Arron <arronzhang@me.com>
2013-09-09 21:53:12 +0800

    Add setting

--- build/service.customer.mk
+++ build/service.customer.mk
@@ -3,10 +3,12 @@ include build/config.mk
 PRODUCT_NAME = service.customer
 
 JS_APP_FILES = ${JS_SRC_DIR}/ui.buddy.js\
+	${JS_SRC_DIR}/ui.setting.js\
 	${JS_SRC_DIR}/ui.user.js\
 	${APP_SRC_DIR}/ui.layout.popup.js\
 
 CSS_APP_FILES = ${CSS_SRC_DIR}/buddy.css\
+	${CSS_SRC_DIR}/setting.css\
 	${CSS_SRC_DIR}/user.css\
 	${APP_SRC_DIR}/layout.popup.css\
 
